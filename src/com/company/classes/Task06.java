76ac21ead1c6c5641364c5093585b115b108ffd2
Makc <mks59k@gmail.com>
2019-06-22 13:09:27 +0300

    Метод Start()

--- src/com/company/classes/Task06.java
+++ src/com/company/classes/Task06.java
@@ -10,6 +10,62 @@ package com.company.classes;
 // - Сравнить скорости работы.
 
 public class Task06 {
+
+    private String text = "";
+    private String regex = "";
+    private long elapsedTime;
+
+    public Task06(){
+        text = "Ссылка 1: site.com" +
+                ", ссылка 2: www.site2.com" +
+                ", ссылка 3: http://site3.com" +
+                ", ссылка 4: https://site4.com" +
+                ", email: mail@site.com";
+        regex = "(" +
+                "([a-qA-Q0-9]*@)" +
+                "|([a-qA-Q0-9]*)" +
+                "|((http|ftp|https):\\/\\/)" +
+                ")" +
+                "([\\w_-]+(?:(?:\\.[\\w_-]+)+))" +
+                "([\\w.,@?^=%&:\\/~+#-]*[\\w@?^=%&\\/~+#-])?";
+    }
     public void Start() {
+        System.out.println("= Дана строка:");
+        System.out.println(text);
+
+        System.out.println("\n--- Метод с помощью регулярных выражений.");
+        System.out.println("= Строка после замены ссылок и email на *****:");
+        System.out.println(ReplaceLinksEmailWithRegex(text));
+        System.out.print("= Время выполнения метода: " + elapsedTime + "мс.");
+
+
+        System.out.println("\n\n--- Метод без регулярных выражений.");
+        System.out.println("= Строка после замены ссылок и email на *****:");
+        System.out.println(ReplaceLinksEmail(text));
+        System.out.print("= Время выполнения метода: " + elapsedTime + "мс.");
+    }
+
+    private String ReplaceLinksEmailWithRegex(String str) {
+
+        long startTime = System.currentTimeMillis();
+
+        //
+
+        long stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+
+        return "TODO: строку передать";
+    }
+
+    private String ReplaceLinksEmail(String str) {
+
+        long startTime = System.currentTimeMillis();
+
+        //
+
+        long stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+
+        return "TODO: строку передать";
     }
 }
